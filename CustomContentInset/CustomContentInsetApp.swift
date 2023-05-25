8d8dd0e32273ee308f2ef8159d1d4a2a18f0d2ac
Ramill <ramill_ibragimov@icloud.com>
2023-05-25 19:03:51 +0300

    Initial Commit

---
+++ CustomContentInset/CustomContentInsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomContentInsetApp.swift
+//  CustomContentInset
+//
+//  Created by Ramill Ibragimov on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomContentInsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
